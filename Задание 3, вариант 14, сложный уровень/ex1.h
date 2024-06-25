a0a7f59385d86930011ff4b586f3e7a300591004
miktory <103301849+miktory@users.noreply.github.com>
2024-06-25 14:25:32 +0300

    Add files via upload

---
+++ Задание 3, вариант 14, сложный уровень/ex1.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+// Задание 3_1
+namespace ex1
+{
+	const char* getSymbols(const char* str);
+	char* resize(const char* str, unsigned size, unsigned new_size);
+}
