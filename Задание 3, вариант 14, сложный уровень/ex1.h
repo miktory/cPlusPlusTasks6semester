6812e36dc20e93a05520001310bbdde8b61ee679
miktory <103301849+miktory@users.noreply.github.com>
2024-06-25 14:28:18 +0300

    Delete Задание 3, вариант 14, сложный уровень directory

--- Задание 3, вариант 14, сложный уровень/ex1.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-
-// Задание 3_1
-namespace ex1
-{
-	const char* getSymbols(const char* str);
-	char* resize(const char* str, unsigned size, unsigned new_size);
-}
